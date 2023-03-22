9663d5e28a2a0c5eea418743887ee056c2520853
abelacco <abel3121@gmail.com>
2023-03-22 10:13:50 -0500

    ip mac y pcname

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ wifi.init({
 });
 
 const app = express();
+const os = require('os');
+const dns = require('dns');
 const port = 3000;
 
 // Ruta para obtener la información de las redes Wi-Fi
@@ -21,6 +23,33 @@ app.get('/wifi', (req, res) => {
       res.send(networks);
     }
   });
+});
+
+
+app.get('/info', (req, res) => {
+    const hostname = os.hostname();
+    const networkInterfaces = os.networkInterfaces();
+    let ipAddress;
+    let macAddress;
+  
+    // Encuentra la dirección IP y la dirección MAC de la interfaz de red activa
+    for (const key in networkInterfaces) {
+      const iface = networkInterfaces[key];
+      for (const item of iface) {
+        if (!item.internal && item.family === 'IPv4') {
+          ipAddress = item.address;
+          macAddress = item.mac;
+          break;
+        }
+      }
+      if (ipAddress && macAddress) break;
+    }
+  
+    res.send({
+      hostname,
+      ipAddress,
+      macAddress,
+    });
 });
 
 app.listen(port, () => {
